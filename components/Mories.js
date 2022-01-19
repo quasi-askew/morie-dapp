45cec2c5eacd180ab79da328638c8def441863ca
Sam Fortin <sam.fortin@irishtitan.com>
2022-01-18 22:48:11 -0600

    apollo check

--- components/Mories.js
+++ components/Mories.js
@@ -1,94 +1,27 @@
 import NextImage from "next/image";
-import randomColor from "randomcolor";
-import MorieQuery from "../utils/MorieQuery";
-import { withUrqlClient, initUrqlClient } from "next-urql";
-import {
-  ssrExchange,
-  dedupExchange,
-  cacheExchange,
-  fetchExchange,
-  useQuery,
-} from "urql";
 
-const Mories = ({ skip, first }) => {
-  const [result, reexecuteQuery] = useQuery({
-    query: MorieQuery,
-    variables: {
-      skip: skip,
-      first: first,
-    },
-  });
+const Mories = ({ mories }) => {
 
-  const { data, fetching, error } = result;
-
-  const keyStr =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-  const triplet = (e1, e2, e3) =>
-    keyStr.charAt(e1 >> 2) +
-    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-    keyStr.charAt(e3 & 63);
-
-  const rgbDataURL = (rbg) => {
-    return `data:image/gif;base64,R0lGODlhAQABAPAA${
-      triplet(0, rbg[0], rbg[2]) + triplet(rbg[1], 255, 255)
-    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
-  };
-
-  if (!data) {
+  if (!mories) {
     return null;
   }
 
   return (
     <>
-      {data?.tokens &&
-        data.tokens.map((token) => {
+      {mories &&
+        mories.map((token) => {
           return (
-            <div key={token.tokenID}>
-              <NextImage
-                height={75}
-                width={75}
-                src={token.imageURI}
-                alt={token.tokenID}
-                placeholder="blur"
-                blurDataURL={rgbDataURL(
-                  randomColor({
-                    luminosity: "light",
-                    format: "rgbArray",
-                  })
-                )}
-              />
-            </div>
+            <NextImage
+              key={token.tokenID}
+              height={40}
+              width={40}
+              src={token.imageURI}
+              alt={token.tokenID}
+            />
           );
         })}
     </>
   );
 };
 
-export async function getStaticProps(ctx) {
-  const ssrCache = ssrExchange({ isClient: false });
-  const client = initUrqlClient({
-    url: "https://api.thegraph.com/subgraphs/name/quasi-askew/world-of-iwwon",
-    exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
-  });
-
-  // This query is used to populate the cache for the query
-  // used on this page.
-  await client.query(MorieQuery).toPromise();
-
-  return {
-    props: {
-      // urqlState is a keyword here so withUrqlClient can pick it up.
-      urqlState: ssrCache.extractData(),
-    },
-    revalidate: 600,
-  };
-}
-
-export default withUrqlClient(
-  (ssr) => ({
-    url: "https://api.thegraph.com/subgraphs/name/quasi-askew/world-of-iwwon",
-  }),
-  { ssr: false } // Important so we don't wrap our component in getInitialProps
-)(Mories);
+export default Mories;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,48 @@
 import Head from "next/head";
 import Mories from "../components/Mories";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
-const Home = () => {
+const Home = ({
+  morieWave1,
+  morieWave2,
+  morieWave3,
+  morieWave4,
+  morieWave5,
+  morieWave6,
+  morieWave7,
+  morieWave8,
+  morieWave9,
+  morieWave10,
+}) => {
+  const allMories = [
+    ...morieWave1,
+    ...morieWave2,
+    ...morieWave3,
+    ...morieWave4,
+    ...morieWave5,
+    ...morieWave6,
+    ...morieWave7,
+    ...morieWave8,
+    ...morieWave9,
+    ...morieWave10,
+  ];
+
   return (
     <div>
       <Head>
         <title>mementomories</title>
         <meta
           name="description"
-          content='"memento mori" is a latin saying that means"Remember you die".'
+          content='"Memento Mori" is a latin saying that means "Remember you die".'
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            flexWrap: "wrap",
-            maxHeight: "100vh",
-          }}
         >
-          <Mories skip={0} first={1000} />
-          <Mories skip={1000} first={1000} />
-          <Mories skip={2000} first={1000} />
-          <Mories skip={3000} first={1000} />
-          <Mories skip={4000} first={1000} />
-          <Mories skip={5000} first={1000} />
-          <Mories skip={6000} first={1000} />
-          <Mories skip={7000} first={1000} />
-          <Mories skip={8000} first={1000} />
-          <Mories skip={9000} first={1000} />
+          <Mories mories={allMories} />
         </div>
       </main>
     </div>
@@ -39,3 +50,130 @@ const Home = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const { data: morieWave1 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 0 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave2 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 1000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave3 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 2000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave4 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 3000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave5 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 4000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave6 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 5000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave7 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 6000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave8 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 7000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave9 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 8000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  const { data: morieWave10 } = await client.query({
+    query: gql`
+      query {
+        tokens(orderBy: tokenID, first: 1000, where: { tokenID_gt: 9000 }) {
+          tokenID
+          imageURI
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      morieWave1: morieWave1.tokens,
+      morieWave2: morieWave2.tokens,
+      morieWave3: morieWave3.tokens,
+      morieWave4: morieWave4.tokens,
+      morieWave5: morieWave5.tokens,
+      morieWave6: morieWave6.tokens,
+      morieWave7: morieWave7.tokens,
+      morieWave8: morieWave8.tokens,
+      morieWave9: morieWave9.tokens,
+      morieWave10: morieWave10.tokens,
+    },
+  };
+}
